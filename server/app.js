a798e5b3d7a2ab508dc24a820da342619a55346a
elynefdc <elyne.fdc1@gmail.com>
2023-10-04 00:35:14 -0400

    Ajouts dans \server\app.js

--- server/app.js
+++ server/app.js
@@ -10,10 +10,51 @@ const db = require('./queries')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
+
 app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
+  schema,
+  graphiql: true
 }));
+
+//begin added
+app.get('/kanban', async (req, res) => {
+  try {
+    const response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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', 
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+          repository(owner: "Horion31", name: "log680-grp1-eq20-e23") {
+            name,
+            projects (first : 1) { nodes {name} }
+          }
+        }
+             
+        `
+      }),
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
+  }
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur la page d'accueil de l'application !");
+});
+
+//end added
+
 
 
 app.listen(port, () => {
