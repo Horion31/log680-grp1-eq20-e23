0a0794ae89b04b46dd143bc6d52148ae3a0816f3
elynefdc <elyne.fdc1@gmail.com>
2023-10-05 22:15:51 -0400

    Ajouts métriques Pull Request

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ const nomTache  = "Créer une API"
 const NomColonne = "A faire"
 
 //Variables définissant la période choisie (metrique 2 et 4)
-const dateFin = new Date("2023-11-03");
+const dateFin = new Date("2023-12-03");
 const dateDebut = new Date("2023-09-01");
 
 
@@ -48,7 +48,7 @@ const baseUrl = "https://api.github.com/graphql";
 
 const headers = {
     "Content-Type": "application/json",
-    authorization: "bearer " + "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" //CHANGER TOKEN
+    authorization: "bearer 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" //CHANGER TOKEN
 };
 
 //requete metriques
@@ -78,38 +78,46 @@ const query1 = {
   `,
 };
 
-const query2 = {
-    query: ` query{
-        user(login: "${process.env.GITHUB_USERNAME}") {
-        issues(last: 100, orderBy: {field:CREATED_AT, direction: DESC}){
+const queryPullRequest = {
+  query: ` query {
+repository(owner: "Horion31", name: "log680-grp1-eq20-e23") {
+  name
+  owner {
+    login
+  }
+  description
+  createdAt
+  updatedAt
+  stargazerCount
+  forkCount
+  watchers {
+    totalCount
+  }
+  issues {
+    totalCount
+  }
+  pullRequests(first: 5) {
+    totalCount
+    nodes {
+      number
+      title
+      createdAt
+      updatedAt
+      closedAt
+      author {
+        login
+      }
+      body
+      state
+      comments {
         totalCount
-        nodes{
-            id
-            closed
-            title
-            createdAt
-          url
-          number
-          assignees(first:100){
-            nodes{
-              avatarUrl
-              name
-              url
-            }
-          }
-          repository{
-            name
-            url
-            owner{
-              login
-              avatarUrl
-              url
-            }
-          }
-        }
       }
     }
-      }`,
+  }
+}
+}
+
+  `,
 };
 
 
@@ -262,6 +270,177 @@ console.log(`Le nombre de tâches terminées pour la periode de ${dateDebut} à 
     res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
   }
 });
+
+//routes Pull Request
+//metrique1 : temps de réaction après le lancement de la pull request
+app.get('/pullrequest/metrique1', async (req, res) => {
+  try {
+      const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(queryPullRequest),
+    });
+    
+    const data = await response.json();
+    let i = 0;
+
+    while (i < data.data.repository.pullRequests.nodes.length) {
+    const pullRequest = data.data.repository.pullRequests.nodes[i];
+    const createdAt = new Date(pullRequest.createdAt);
+    const updatedAt = new Date(pullRequest.updatedAt);
+    const timeDiff = (updatedAt - createdAt) / 1000; 
+
+    console.log(`La pull-request appelée "${pullRequest.title}" a été révisée la première fois au bout de ${timeDiff} secondes.`);
+    
+  i++;
+  }
+
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
+  }
+});
+
+//metrique2 : temps de fusion des pull request
+app.get('/pullrequest/metrique2', async (req, res) => {
+  try {
+      const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(queryPullRequest),
+    });
+    
+    const data = await response.json();
+    let i = 0;
+
+    while (i < data.data.repository.pullRequests.nodes.length) {
+      const pullRequest = data.data.repository.pullRequests.nodes[i];
+      const createdAt = new Date(pullRequest.createdAt);
+      const closedAt = new Date(pullRequest.closedAt);
+      if (pullRequest.closedAt === null) {
+        const timeDiff = (dateActuelle - createdAt) / 1000;
+        console.log(`le temps de fusion pour la pull request appelée "${pullRequest.title}" est : ${timeDiff} secondes `);
+      }
+      else {
+        const timeDiff2 = (closedAt - createdAt) / 1000;
+        console.log(`le temps de fusion pour la pull request appelée "${pullRequest.title}" est : ${timeDiff2} secondes `);
+      }
+      
+      i++;
+    }
+
+
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
+  }
+});
+
+
+//metrique3 : nombre de pull requests actives pour une période donnée
+app.get('/pullrequest/metrique3', async (req, res) => {
+  try {
+      const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(queryPullRequest),
+    });
+    
+    const data = await response.json();
+    
+    let compteurPR = 0;
+    let i = 0;
+
+    while (i < data.data.repository.pullRequests.nodes.length) {
+      const pullRequest = data.data.repository.pullRequests.nodes[i];
+      const createdAt = new Date(pullRequest.createdAt);
+      const closedAt = new Date(pullRequest.closedAt);
+      if ((createdAt <= dateFin) && ((closedAt >= dateFin)||(pullRequest.closedAt == null))) {
+        compteurPR++;
+      }
+      i++;
+    }
+console.log(`Le nombre de pull request actives pour la periode de ${dateDebut} à ${dateFin} est : ${compteurPR}`);
+
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
+  }
+});
+
+//metrique4 : nombre de commentaires pour les pull requests pour une période donnée
+app.get('/pullrequest/metrique4', async (req, res) => {
+  try {
+      const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(queryPullRequest),
+    });
+    
+    const data = await response.json();
+    
+    let compteurPR = 0;
+    let i = 0;
+
+    while (i < data.data.repository.pullRequests.nodes.length) {
+      const pullRequest = data.data.repository.pullRequests.nodes[i];
+      const createdAt = new Date(pullRequest.createdAt);
+      const closedAt = new Date(pullRequest.closedAt);
+      if ((createdAt <= dateFin) && ((closedAt >= dateFin)||(pullRequest.closedAt == null))) {
+        console.log(`Le nombre de commentaires pour la pull request appelée "${pullRequest.title}" pour la periode de ${dateDebut} à ${dateFin} est : "${pullRequest.comments.totalCount}"`);
+      }
+      i++;
+    }
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
+  }
+});
+
+//metrique5 : taux de succès des pull request (pourcentage de pull reuqests qui sont fusionnées par rapport au nombre total de pull requests ouvertes)
+app.get('/pullrequest/metrique5', async (req, res) => {
+  try {
+      const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(queryPullRequest),
+    });
+    
+    const data = await response.json();
+    
+    let compteurOpen = 1;
+    let compteurMerged = 0;
+    let i = 0;
+
+    while (i < data.data.repository.pullRequests.nodes.length) {
+      const pullRequest = data.data.repository.pullRequests.nodes[i];
+      if (pullRequest.state === "OPEN") {
+        compteurOpen++;
+      }
+      else if (pullRequest.state === "MERGED") {
+        compteurMerged++;
+      }
+      else {}
+      i++;
+    }
+    const taux = (compteurMerged / compteurOpen)*100;
+
+    console.log(`Le taux de succès des pull request est de : "${taux}%"`);
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données du tableau Kanban.' });
+  }
+});
+
 
 app.get("/", (req, res) => {
     res.send("Bienvenue sur la page d'accueil de l'application !");
