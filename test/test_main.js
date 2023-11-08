8390800606aba084ebcbae3caaca8d80cc72fa62
elynefdc <144647580+elynefdc@users.noreply.github.com>
2023-11-07 22:49:51 -0500

    Update test_main.js

--- test/test_main.js
+++ test/test_main.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./server/app'); // Assurez-vous d'importer correctement votre application
 
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('App', () => {
+  it('should respond with a welcome message at the root URL', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal("Bienvenue sur la page d'accueil de l'application !");
+        done();
+      });
+  });
+
+  it('should return a 404 status code for undefined routes', (done) => {
+    chai.request(app)
+      .get('/undefinedroute')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+});
