0606af58066d1f24cc834607c984910aff39e64f
elynefdc <144647580+elynefdc@users.noreply.github.com>
2023-11-07 21:47:41 -0500

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// index.js
+const module1 = require('./app');
+const module2 = require('./db_app');
+const module2 = require('./db_sequalize');
+
+module.exports = {
+  app,
+  db_app,
+  db_sequalize,
+};
