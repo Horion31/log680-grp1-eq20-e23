f87ebf310e784a087d0a162b99cbea5a5468d853
elynefdc <144647580+elynefdc@users.noreply.github.com>
2023-11-09 01:49:23 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ require('dotenv').config()
 
 const app = express()
 const port = 3000
-module.exports = app;
+
 
 db_util.syncDataBase();
 
@@ -1021,3 +1021,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+module.exports = app;
