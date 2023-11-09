3a6a0f242fd2e2569ed848ce872ca94c40eb3feb
elynefdc <144647580+elynefdc@users.noreply.github.com>
2023-11-09 01:42:33 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 // index.js
-const module_app = require('./app');
+/*const module_app = require('./app');
 const module2 = require('./db_app');
 const module2 = require('./db_sequalize');
 
 module.exports = {
   module_app,
 };
+*/
