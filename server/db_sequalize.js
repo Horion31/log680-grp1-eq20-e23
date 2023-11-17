ff458a20f4023239d4fd361fc59384ebd8db04f9
Jean Lamarre <jonny.lamarre@hotmail.com>
2023-11-17 12:36:17 -0500

    wip dev-jean

--- server/db_sequalize.js
+++ server/db_sequalize.js
@@ -2,7 +2,7 @@ const { Sequelize, DataTypes, Model } = require('sequelize')
 
 const sequelize = new Sequelize({
     host: 'host.docker.internal',
-    database: 'db_postgres',
+    database: 'db_oxygen',
     username: 'postgres',
     password: 'postgres',
     port: 5432,
